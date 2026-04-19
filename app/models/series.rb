ee75a6c9c9950c3efe5b99c01a8668689dcac521
Rebeca Araújo <rebecaaraujo2013@gmail.com>
2026-04-19 00:39:16 -0300

    add tests to validate creation of series
    Validates presence of all columns,  remove fixtures to make data visualization better in the tests.

--- app/models/series.rb
+++ app/models/series.rb
@@ -1,2 +1,3 @@
 class Series < ApplicationRecord
+  validates :country_code, :country_name, :frequency, :series_type, :basket, presence: true
 end
--- test/models/series_test.rb
+++ test/models/series_test.rb
@@ -4,12 +4,55 @@ class SeriesTest < ActiveSupport::TestCase
   test "should add one series" do
     assert_difference("Series.count", 1) do
       Series.create(
-        country_code: "BR",
-        country_name: "Brazil",
-        frequency: "Monthly",
-        series_type: "Nominal",
-        basket: "Broad"
-      )
+        {
+          country_code: "CA",
+          country_name: "Canada",
+          frequency: "Monthly",
+          series_type: "Real",
+          basket: "Broad"
+        })
     end
+  end
+
+  test "should not save series without data" do
+    no_code = Series.new(
+      country_name: "Brazil",
+      frequency: "Monthly",
+      series_type: "Nominal",
+      basket: "Narrow"
+    )
+    assert_not no_code.save, "Saved series without country_code"
+
+    no_country_name = Series.new(
+      country_code: "BR",
+      frequency: "Monthly",
+      series_type: "Nominal",
+      basket: "Narrow"
+    )
+    assert_not no_country_name.save, "Saved series without country_name"
+
+    no_frequency = Series.new(
+      country_name: "Brazil",
+      country_code: "BR",
+      series_type: "Nominal",
+      basket: "Narrow"
+    )
+    assert_not no_frequency.save, "Saved series without frequency"
+
+    no_series_type = Series.new(
+      country_name: "Brazil",
+      country_code: "BR",
+      frequency: "Monthly",
+      basket: "Narrow"
+    )
+    assert_not no_series_type.save, "Saved series without series_type"
+
+    no_basket = Series.new(
+      country_name: "Brazil",
+      country_code: "BR",
+      frequency: "Monthly",
+      series_type: "Nominal",
+    )
+    assert_not no_basket.save, "Saved series without basket"
   end
 end
