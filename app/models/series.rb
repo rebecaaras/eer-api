4b0e41b566685f4e1384af22ed1bbcdd61a1a4be
Rebeca Araújo <rebecaaraujo2013@gmail.com>
2026-04-22 19:29:47 -0300

    add ! to normalize_attributes  for it is a bang method and make it return nil

--- app/models/series.rb
+++ app/models/series.rb
@@ -1,12 +1,15 @@
 class Series < ApplicationRecord
-  before_save :normalize_attributes
   validates :country_code, :country_name, :frequency, :series_type, :basket, presence: true
 
-  def normalize_attributes
-    self.country_code = country_code&.upcase
+  before_save :normalize_attributes!
+
+  def normalize_attributes!
+    self.country_code = country_code.upcase
     self.country_name = country_name&.downcase
     self.frequency = frequency&.downcase
     self.series_type = series_type&.downcase
-    self.basket = basket.downcase
+    self.basket = basket&.downcase
+
+    nil
   end
 end
